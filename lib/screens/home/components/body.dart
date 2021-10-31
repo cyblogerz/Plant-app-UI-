ca0a363fbecfa96ffa20eaac9e880a989b9509c8
cyblogerz <pranavajay2002@gmail.com>
2021-10-31 17:03:55 +0530

    Created header with search bar

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,1 +1,21 @@
+import 'package:app1/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'header_with_search.dart';
+
+class Body extends StatelessWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          HeaderwithSearch(size: size),
+        ],
+      ),
+    );
+  }
+}
